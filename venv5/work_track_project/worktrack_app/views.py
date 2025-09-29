dde68092d5695b72199da48adc3ac9f2dc4da0a7
jalal447 <jalaludheen3113@gmail.com>
2025-09-29 12:22:12 +0530

    second commit

--- venv5/work_track_project/worktrack_app/views.py
+++ venv5/work_track_project/worktrack_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+
+
 
 # Create your views here.
+def index(request):
+    return HTTPResponse('hii how are u')
+
+def sample(request):
+    return HTTPResponse('test failed')
+
+#push
